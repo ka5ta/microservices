492f171dda16d9072cbeb28fda6d6b7dbdb50a15
ka5ta <55314237+ka5ta@users.noreply.github.com>
2024-10-18 09:47:33 +0200

    remove ip adresses from OpenFeign

--- product-service/src/main/java/com/example/productservice/service/CatalogServiceClient.java
+++ product-service/src/main/java/com/example/productservice/service/CatalogServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@FeignClient(value = "catalog-service", url = "http://localhost:8081")
+@FeignClient(value = "catalog-service")
 public interface CatalogServiceClient {
 
     @GetMapping("/products/{id}")
--- product-service/src/main/java/com/example/productservice/service/InventoryServiceClient.java
+++ product-service/src/main/java/com/example/productservice/service/InventoryServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(value = "inventory-service", url = "http://localhost:8082")
+@FeignClient(value = "inventory-service")
 public interface InventoryServiceClient {
 
 
